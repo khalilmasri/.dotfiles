9e5ebfa043c60efed881d4c565502245def3119e
Khalil <khalil@pop-os.localdomain>
2021-08-02 18:03:50 +0300

    Install files script

---
+++ .install.sh
@@ -0,0 +1,22 @@
+# install vim
+sudo apt install vim
+
+# zsh and tmux
+sudo apt install zsh
+sudo apt update zsh
+sudo apt install -y fonts-font-awesome
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+sudo apt install tmux
+
+git clone https://github.com/khalilmasri/.dotfiles.git
+
+tmux source .tmux.conf
+chsh
