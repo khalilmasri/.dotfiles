d810e2b249643ea280e0139ea44c392a07772953
khalilmasri <khalil.1996km@gmail.com>
2021-08-12 21:07:39 +0300

    Install script

--- .install.sh
+++ .install.sh
@@ -65,3 +65,11 @@ sudo apt install fd-find
 sudo apt install check
 sudo ln -s /usr/local/lib/libcheck.so.0 /usr/lib/libcheck.so.0
 
+# Install colorscript
+git clone https://gitlab.com/dwt1/shell-color-scripts.git
+cd shell-color-scripts
+rm -rf /opt/shell-color-scripts || return 1
+sudo mkdir -p /opt/shell-color-scripts/colorscripts || return 1
+sudo cp -rf colorscripts/* /opt/shell-color-scripts/colorscripts
+sudo cp colorscript.sh /usr/bin/colorscript
+
